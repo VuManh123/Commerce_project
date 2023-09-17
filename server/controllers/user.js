b2b9e12492ec503cb4cf7895c5e9f927838d2cd9
Vumanh-mvp <vuducmanh10a@gmail.com>
2023-09-17 17:38:49 +0700

    #5

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,7 @@ const User = require('../models/user')
 
 const asyncHandler = require('express-async-handler')
 
-const{generateAccessToken} = require('../middlewares/jwt')
+const{generateAccessToken, generateRefreshToken} = require('../middlewares/jwt')
 
 // Đăng kí
 const register = asyncHandler(async(req, res) => {
@@ -33,6 +33,8 @@ const register = asyncHandler(async(req, res) => {
 })
 
 // Đăng nhập 
+// RefreshToken => lam moi access Token
+// Access Token => Xac thuc nguoi dung, quan ly nguoi dung
 const login = asyncHandler(async(req, res) => {
     const {email, password} = req.body
     if(!email || !password)
@@ -43,9 +45,16 @@ const login = asyncHandler(async(req, res) => {
 
     const response = await User.findOne({email})
     if (response && await response.isCorrectPassword(password)) {
-        
+        // Tach password va role ra khoi response
         const { password, role, ...userData } = response.toObject()
+        // Tao accessToken
         const accessToken = generateAccessToken(response._id, role)
+        // Tao refresg Token
+        const refreshToken = generateRefreshToken(response._id)
+        // Luu refreshToken vao database
+        await User.findByIdAndUpdate(response._id, {refreshToken}, {new: true})
+        // Luu refresg Token vao cookie
+        res.cookie('refreshToken', refreshToken, {httpOnly: true, maxAge: 7*24*60*60*1000})
         return res.status(200).json({
             sucess: true,
             accessToken,
@@ -56,7 +65,17 @@ const login = asyncHandler(async(req, res) => {
     }
 })
 
+const getCurrent = asyncHandler(async(req, res) => {
+    const {_id} = req.user
+    const user = await User.findById(_id).select('-refreshToken -password -role')
+    return res.status(200).json({
+        success: false,
+        rs: user ? user: 'User not found'
+    })
+})
+
 module.exports = {
     register,
-    login
+    login,
+    getCurrent
 }
--- server/middlewares/jwt.js
+++ server/middlewares/jwt.js
@@ -1,8 +1,12 @@
 // Token
 const jwt = require('jsonwebtoken')
 
+// Băm data
 const generateAccessToken = (uid, role) => jwt.sign({_id: uid, role}, process.env.JWT_SECRET, {expiresIn: '3d'})
 
+const generateRefreshToken = (uid) => jwt.sign({_id: uid}, process.env.JWT_SECRET, {expiresIn: '7d'})
+
 module.exports = {
-    generateAccessToken
+    generateAccessToken,
+    generateRefreshToken
 }
